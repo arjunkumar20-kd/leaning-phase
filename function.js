3c96255be5f8519f3804c6ad17c846bc7b20cc9a
Arjun Kumar <arjunk840095@gmail.com>
2026-09-23 06:35:39 +0530

    function.html

---
+++ function.js
@@ -0,0 +1,28 @@
+function addNumbers(firstNumber , secondNumber){
+    console.log(firstNumber+secondNumber);
+}
+addNumbers(20,30);
+
+
+function subtract(firstNumber,secondNumber){
+    console.log(firstNumber-secondNumber)
+}
+subtract(5,55);
+
+
+function multiply(firstNumber,secondNumber){
+    console.log(firstNumber*secondNumber);
+}
+multiply(20,30);
+
+
+function divide(firstNumber,secondNumber){
+    console.log(firstNumber/secondNumber)
+}
+divide(5,55);
+
+
+function modulo(firstNumber,secondNumber){
+    console.log(firstNumber%secondNumber)
+}
+modulo(5,55);
